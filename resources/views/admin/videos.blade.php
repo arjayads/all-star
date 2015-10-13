9337fc4d1e47b3bfe61fc70733b534e671820aac
Arjay Ads <arjay@verticalops.com>
2015-10-14 06:02:57 +0800

    add video link to full mode

--- resources/views/admin/videos.blade.php
+++ resources/views/admin/videos.blade.php
@@ -8,12 +8,11 @@
         <div class="col-md-7 col-md-offset-2">      
             @foreach($videos as $video)
                 <div class="col-md-4">                  
-                    <div><h5 for="{{$video->title}}"> {{$video->title}}</h5></div>
-
+                    <div><a href="/admin/videos/{{$video->id}}"><h5 for="{{$video->title}}"> {{$video->title}}</h5></a></div>
                     <div align="center" class="embed-responsive embed-responsive-4by3">
                         <video controls class="embed-responsive-item">
-                            <source src="/admin/videos/1" type="video/mp4">
-                        </video>                    
+                            <source src="/admin/videos/{{$video->id}}" type="{{$video->mime_type}}">
+                        </video>
                     </div>
                     <div>
                         <label><a href="{!! URL::to('/admin/videos/'.$video->id) !!}/edit" >Edit</a></label> | <label><a href="" class="delete" data-id="{!! $video->id !!}" >Delete</a></label>
