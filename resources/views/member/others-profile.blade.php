6b38263429826f0d19220cee01ce3c9734e9fdea
Arjay Ads <arjay@verticalops.com>
2015-10-23 23:58:31 +0800

    init org chart

--- resources/views/member/others-profile.blade.php
+++ resources/views/member/others-profile.blade.php
@@ -1,5 +1,18 @@
 @extends('layout.app')
 
+@section('css')
+    <style>
+        .google-visualization-orgchart-table {
+            border-collapse: inherit !important;
+            width: 100% !important;
+        }
+
+        .google-visualization-orgchart-connrow-medium {
+            height: 25px !important;
+        }
+    </style>
+@stop
+
 @section('content')
     <div class="container-fluid  top-buffer">
         <div class="row">
@@ -34,6 +47,16 @@
                             </p>
                         </div>
                     </div>
+
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Team</h3>
+                        </div>
+                        <div class="panel-body">
+                            <script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1.1','packages':['orgchart']}]}"></script>
+                            <div id="chart_div"></div>
+                        </div>
+                    </div>
                 @endif
             </div>
         </div>
@@ -43,6 +66,35 @@
 
 @section('js')
 <script>
+
+    google.setOnLoadCallback(drawChart);
+    function drawChart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Name');
+        data.addColumn('string', 'Manager');
+        data.addColumn('string', 'ToolTip');
+
+
+        var array = [
+            [{v:'Mike', f:'Mike<div style="color:red; font-style:italic">President</div>'}, '', 'The President'],
+            [{v:'Jim', f:'Jim<div style="color:red; font-style:italic">Vice President</div>'}, 'Mike', 'VP'],
+            [{v:'Jim1', f:'Jim<div style="color:red; font-style:italic">Vice President</div>'}, 'Mike', 'VP'],
+            [{v:'Jim2', f:'Jim<div style="color:red; font-style:italic">Vice President</div>'}, 'Mike', 'VP']
+        ];
+
+        data.addRows(array);
+        var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
+        chart.draw(data, {allowHtml:true});
+
+
+        if (array.length <= 2) {
+            $('.google-visualization-orgchart-table').css('max-width', "30%");
+        } else
+        if (array.length <= 3) {
+            $('.google-visualization-orgchart-table').css('max-width', "50%");
+        }
+    }
+
     var $userId = '{{$member->id}}';
     $('#add').on('click', function() {
         $.post( "/member/requestAdd", { userId: $userId, '_token': '{{csrf_token()}}'}).done(function( data ) {
@@ -63,5 +115,7 @@
             }
         });
     });
+
+
 </script>
 @stop
