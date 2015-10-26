330ab093fd0e05cd5e9c3f5e46eeeb6acc145c4d
Arjay Ads <arjay@verticalops.com>
2015-10-26 22:24:37 +0800

    update color scheme

--- resources/views/member/others-profile.blade.php
+++ resources/views/member/others-profile.blade.php
@@ -73,10 +73,10 @@
                 $('#chart_div').addClass('alert alert-info').text("No team members yet!");
             } else {
                 var rows = [
-                    [{v:$userId, f:$nyMame + '<div class="member" style="color:blue; font-style:italic">' + $nyEmail + '</div>'}, '']
+                    [{v:$userId, f:$nyMame + '<div class="member" style="font-style:italic">' + $nyEmail + '</div>'}, '']
                 ];
                 for(var idx=0; idx<d.length; idx++) {
-                    var member = [{v: d[idx].name, f: d[idx].name + '<div class="' + (d[idx].social_id != '' ? 'member' : '') +'" style="color:blue; font-style:italic">' +  d[idx].email +'</div>'}, $userId]
+                    var member = [{v: d[idx].name, f: d[idx].name + '<div class="' + (d[idx].social_id != '' ? 'member' : 'non-member') +'" style="font-style:italic">' +  d[idx].email +'</div>'}, $userId]
                     rows.push(member);
                 }
 
@@ -93,6 +93,7 @@
                 }
 
                 $('.member').closest('td').removeClass('google-visualization-orgchart-node').addClass('google-visualization-orgchart-node-red');
+                $('.non-member').closest('td').removeClass('google-visualization-orgchart-node').addClass('google-visualization-orgchart-node-blue');
             }
 
         });
