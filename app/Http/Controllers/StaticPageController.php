102b916c676afe143eb2f4e9b08a628fb0b1c86c
Arjay Ads <arjay@verticalops.com>
2015-11-03 00:58:17 +0800

    log out user

--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -8,12 +8,14 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Auth\Guard;
 
 class StaticPageController extends Controller
 {
-    public function __construct(UserRepo $userRepo)
+    public function __construct(UserRepo $userRepo, Guard $auth)
     {
         $this->userRepo = $userRepo;
+        $this->auth = $auth;
     }
 
     function index()
@@ -28,6 +30,7 @@ class StaticPageController extends Controller
 
     public function login()
     {
+        $this->auth->logout();
         return view('login');
     }
 }
