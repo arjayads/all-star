cd3af159305ac4be2f791069b0ba738dab4ed7b2
Arjay Ads <arjay@verticalops.com>
2015-11-04 00:02:10 +0800

    validate user

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -7,7 +7,10 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\Video;
 use App\Repositories\UserRepo;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
 
@@ -30,14 +33,25 @@ class HomeController extends Controller
     }
 
     public function changePassword(Request $request) {
+
         $validator = Validator::make($request->all(), [
             'current_password' => 'required',
-            'password' => 'required|confirmed',
+            'password' => 'required|confirmed|min:6',
         ]);
+
 
         if ($validator->fails()) {
             return redirect('/admin/changePassword')->with('errors', $validator->errors()->all());
+        } else {
+            $credentials = [
+                'email' => Auth::user()->email,
+                'password' => $request->get('current_password')
+            ];
+            $valid = Auth::validate($credentials);
+            if ($valid) {
+                return redirect('/profile');
+            }
+            return redirect('/admin/changePassword')->with('errors', ['Input correct current password']);
         }
-        return redirect('/profile');
     }
 }
